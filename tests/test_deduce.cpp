10d14f416596e5061b32eec852b092e38a06e678
maki49 <1579492865@qq.com>
2023-05-08 19:49:29 +0800

    update test_deduce

--- tests/test_deduce.cpp
+++ tests/test_deduce.cpp
@@ -1,46 +1,72 @@
 #include <gtest/gtest.h>
 #include "../include/opts.hpp"
 #include "../include/deduce.hpp"
+#include "../include/tools.hpp"
 
-class DeduceTest : public testing::Test
+struct Case
+{
+    Case(std::vector<std::string> x, std::vector<std::string> y,
+        std::string result, std::string label_mul = "") :
+        x(x), y(y), result(result), label_mul(label_mul) {}
+    ~Case() {}
+    std::vector<std::string> x;
+    std::vector<std::string> y;
+    std::string result;
+    std::string label_mul="";
+};
+    class DeduceTest : public testing::Test
 {
 protected:
-    std::vector<std::vector<std::string>> case32 = { {"a1", "a2", "b0"}, {"a2", "b1"} };    //32TN
-    std::vector<std::vector<std::string>> case33 = { {"b0", "a1", "b2"}, {"b0", "b1", "b2"} };  //32R32NT
+    std::vector<Case> case_list{
+        Case({"a1", "a2", "b0"}, {"a2", "b1"} , Opts::t3 + Opts::m12 + "TN"),
+        Case({"a0", "b1", "b0"}, {"b0", "b1", "b2"},  Opts::m12 + Opts::to_right + Opts::t3 + Opts::m01 + "NN", "b1b0"),
+        Case({"b0", "a1", "b2"}, {"b0", "b1", "b2"}, Opts::t3 + Opts::m12 + Opts::to_right + Opts::t3 + Opts::m12 + "NT", "b0b2")
+    };
+    // std::vector<std::vector<std::string>> case32 = { {"a1", "a2", "b0"}, {"a2", "b1"} };    //32TN
+    // //std::vector<std::vector<std::string>> case33 = { {"b0", "a1", "b2"}, {"b0", "b1", "b2"} };  //32R32NT
+    // std::vector<std::vector<std::string>> case33 = { {"a0", "b1", "b0"}, {"b0", "b1", "b2"} };  //2R31NN
 };
 
 TEST_F(DeduceTest, deducetest)
 {
-    Deduce deduce("a2");
-
-    //case32
-    std::string trans32 = deduce.if_trans_32(case32[0], case32[1]);
-    if (trans32 == Opts::t3) case32[0] = Opts::label3_trans(case32[0]);
-
-    std::string merge32 = deduce.which_merge_32(case32[0], case32[1]);
-    case32[0] = Opts::label3_merge(case32[0], merge32);
-
-     std::string gemm32 = deduce.trans_in_gemm(case32[0], case32[1]);
-     std::cout << "gemm32=" << gemm32 << std::endl;
-     EXPECT_EQ(trans32 + merge32 + gemm32, Opts::t3 + Opts::m12 + "TN");
-
-    //case 33
-    deduce.set_label_mul("b0b2");
-    std::string trans_merge_33 = deduce.how_trans_merge_33(case33[0], case33[1]);
-    EXPECT_EQ(trans_merge_33, Opts::t3 + Opts::m12 + Opts::to_right + Opts::t3 + Opts::m12);
-
-    std::vector<std::string>* optmat = &case33[0];
-
-    // to do : change this to a subfuntion in Opts: apply_trans_merge_33
-    for (const char s : trans_merge_33)
+    Deduce deduce;
+    for (auto c : case_list)
     {
-        if (s == Opts::t3[0]) *optmat = Opts::label3_trans(*optmat);
-        else if (s == Opts::m01[0]) *optmat = Opts::label3_merge(*optmat, Opts::m01);
-        else if (s == Opts::m12[0]) *optmat = Opts::label3_merge(*optmat, Opts::m12);
-        else if (s == Opts::to_right[0]) optmat = &case33[1];
-        std::cout << "s=" <<s<< std::endl;
+        deduce.set_label_mul(tools::find_common(c.x, c.y));
+        if (c.y.size() == 2)
+        {
+            std::string trans32 = deduce.if_trans_32(c.x, c.y);
+            if (trans32 == Opts::t3) c.x = Opts::label3_trans(c.x);
+
+            std::string merge32 = deduce.which_merge_32(c.x, c.y);
+            c.x = Opts::label3_merge(c.x, merge32);
+
+            std::string gemm32 = deduce.trans_in_gemm(c.x, c.y);
+
+            EXPECT_EQ(trans32 + merge32 + gemm32, c.result);
+        }
+        else if (c.y.size() == 3)
+        {
+            assert(c.label_mul != "");
+            deduce.set_label_mul(c.label_mul);
+            std::string trans_merge_33 = deduce.how_trans_merge_33(c.x, c.y);
+
+            std::vector<std::string>* optmat = &c.x;
+
+            // to do : change this to a subfuntion in Opts: apply_trans_merge_33
+            for (const char s : trans_merge_33)
+            {
+                if (s == Opts::t3[0]) *optmat = Opts::label3_trans(*optmat);
+                else if (s == Opts::m01[0]) *optmat = Opts::label3_merge(*optmat, Opts::m01);
+                else if (s == Opts::m12[0]) *optmat = Opts::label3_merge(*optmat, Opts::m12);
+                else if (s == Opts::to_right[0]) optmat = &c.y;
+                //std::cout << "s=" <<s<< std::endl;
+            }
+            EXPECT_EQ(trans_merge_33 + deduce.trans_in_gemm(c.x, c.y), c.result);
+            break;
+        }
     }
-    EXPECT_EQ(deduce.trans_in_gemm(case33[0], case33[1]), "NT");
+
 }
 
 int main(int argc, char **argv)
